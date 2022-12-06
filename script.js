54d59155f26a6e758943ab3858e95787664fe000
harmoronkallaste <harmo.ron@gmail.com>
2022-12-06 18:24:46 +0200

    KOMMENTEERISIN JS VIITE JA EEMALDASIN EBAVAJALIKU INFO

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Selle JavaSript osa sain järgmiselt lehelt - URL: https://codepen.io/beer39/pen/jOqjXrZ
+// Korrigeerisin sellega seotud HTML ja CSS failid vastavalt meie veebilehe vajadustele ning ühendasin need kõik omavahel ära
+
 function actionToggle() {
 	const action = document.querySelector('.action');
 	action.classList.toggle('active')
